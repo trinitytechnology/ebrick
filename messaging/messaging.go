a2288119bec8acdbd46d1b1ff0b36971d3f1fca9
Tai Bui <buixuantai96@gmail.com>
2024-09-26 14:38:24 +0700

    change subject param in interface message to topic to common term

--- messaging/messaging.go
+++ messaging/messaging.go
@@ -13,9 +13,9 @@ var (
 
 type CloudEventStream interface {
 	Publish(ctx context.Context, ev event.Event) error
-	Subscribe(subject, group string, handler func(msg *event.Event, ctx context.Context) error) error
-	SubscribeDLQ(subject string, handler func(msg any, ctx context.Context) error) error
-	CreateStream(stream string, subjects []string) error
+	Subscribe(topic, group string, handler func(msg *event.Event, ctx context.Context) error) error
+	SubscribeDLQ(topic string, handler func(msg any, ctx context.Context) error) error
+	CreateStream(stream string, topics []string) error
 	CreateConsumerGroup(stream, name string, config ConsumerConfig) error
 	Close() error
 }
